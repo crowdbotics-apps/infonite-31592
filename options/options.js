6fbb89886ad4ca635bbfb50207137f782fb56521
Crowdbotics <team@crowdbotics.com>
2021-10-20 14:19:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "infonite_31592",
+  url: "https://infonite_31592.botics.co",
+  api: "https://infonite_31592.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
